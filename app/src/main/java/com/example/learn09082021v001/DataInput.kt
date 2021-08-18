54d2dcf9818a8b14cd06b8eb2ff6463682066b1e
ss B2002 <phamvantuy93@gmail.com>
2021-08-18 22:44:58 +0700

    ussing_oop

--- app/src/main/java/com/example/learn09082021v001/DataInput.kt
+++ app/src/main/java/com/example/learn09082021v001/DataInput.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_data_input.*
 
 
@@ -12,45 +11,34 @@ class DataInput : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_input)
-        val ScoreOOP = NameScoreOOP()
+        val scoreOOP = NameScoreOOP()
         // BTN SET ONCLICK LISTENNER
         btnINCAndroid.setOnClickListener {
-            if (edtNameAndroid.text.isNotEmpty() && ScoreOOP.ScoreInPutandroid <= 8) {
-                ScoreOOP.ScoreInPutandroid = ScoreOOP.ScoreInPutandroid + 1
-                tvScoreAndroid.text = ScoreOOP.ScoreInPutandroid.toString()
-            } else Toast.makeText(this, "DATA CHANGE FAIL", Toast.LENGTH_SHORT).show()
+            scoreOOP.androiScoreINC()
+            tvScoreAndroid.text = scoreOOP.ScoreInPutandroid.toString()
         }
         btnDECAndroid.setOnClickListener {
-            if (edtNameAndroid.text.isNotEmpty() && ScoreOOP.ScoreInPutandroid >= 1) {
-                ScoreOOP.ScoreInPutandroid = ScoreOOP.ScoreInPutandroid - 1
-                ScoreOOP.ScoreInPutandroid.toString().also { tvScoreAndroid.text = it }
-            } else Toast.makeText(this, "DATA CHANGE FAIL", Toast.LENGTH_SHORT).show()
-
+            scoreOOP.androiScoreDEC()
+            tvScoreAndroid.text = scoreOOP.ScoreInPutandroid.toString()
         }
         btnINCIOS.setOnClickListener {
-            if (edtNameIOS.text.isNotEmpty() && ScoreOOP.ScoreInPutIOS <= 8) {
-                ScoreOOP.ScoreInPutIOS = ScoreOOP.ScoreInPutIOS + 1
-                tvScoreIOS.text = ScoreOOP.ScoreInPutIOS.toString()
-            } else Toast.makeText(this, "DATA CHANGE FAIL", Toast.LENGTH_SHORT).show()
-
+            scoreOOP.IOSScoreINC()
+            tvScoreIOS.text = scoreOOP.ScoreInPutIOS.toString()
         }
         btnDECIOS.setOnClickListener {
-            if (edtNameIOS.text.isNotEmpty() && ScoreOOP.ScoreInPutIOS >= 1) {
-                ScoreOOP.ScoreInPutIOS = ScoreOOP.ScoreInPutIOS - 1
-                tvScoreIOS.text = ScoreOOP.ScoreInPutIOS.toString()
+            scoreOOP.IOSScoreDEC()
+            tvScoreIOS.text = scoreOOP.ScoreInPutIOS.toString() }
 
-            } else Toast.makeText(this, "DATA CHANGE FAIL", Toast.LENGTH_SHORT).show()
-        }
-        btnDone.setOnClickListener {
+            btnDone.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("AndroidName", edtNameAndroid.text.toString())
+                intent.putExtra("IOSName", edtNameIOS.text.toString())
+                intent.putExtra("AndroidScore", tvScoreAndroid.text.toString())
+                intent.putExtra("IOSScore", tvScoreIOS.text.toString())
+                startActivity(intent)
+                Log.d("AAA", "BTN DONE ON")
+            }
 
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("AndroidName", edtNameAndroid.text.toString())
-            intent.putExtra("IOSName", edtNameIOS.text.toString())
-            intent.putExtra("AndroidScore", tvScoreAndroid.text.toString())
-            intent.putExtra("IOSScore", tvScoreIOS.text.toString())
-            startActivity(intent)
-            Log.d("AAA", "BTN DONE ON")
         }
     }
 
-}
--- app/src/main/java/com/example/learn09082021v001/NameScoreOOP.kt
+++ app/src/main/java/com/example/learn09082021v001/NameScoreOOP.kt
@@ -1,9 +1,19 @@
 package com.example.learn09082021v001
 
 class NameScoreOOP {
-    public var NameInPutandroid = ""
-    public var NameInputIOS = ""
     public var ScoreInPutandroid: Int = 0
     public var ScoreInPutIOS: Int = 0
+    fun androiScoreINC (){
+        if (ScoreInPutandroid <= 8 ) ScoreInPutandroid = ScoreInPutandroid + 1
+    }
+    fun androiScoreDEC (){
+        if (ScoreInPutandroid >=1  ) ScoreInPutandroid = ScoreInPutandroid - 1
+    }
+    fun IOSScoreINC (){
+        if (ScoreInPutIOS <= 8 ) ScoreInPutIOS = ScoreInPutIOS + 1
+    }
+    fun IOSScoreDEC (){
+        if (ScoreInPutIOS >=1  ) ScoreInPutIOS = ScoreInPutIOS - 1
+    }
 
 }
