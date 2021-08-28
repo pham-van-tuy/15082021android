bf3a9f6036a8751af8657ef560529d1e2cfef6b7
ss B2002 <phamvantuy93@gmail.com>
2021-08-28 23:06:46 +0700

    rotation run

--- app/src/main/java/com/example/learn09082021v001/DataInput.kt
+++ app/src/main/java/com/example/learn09082021v001/DataInput.kt
@@ -6,29 +6,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import kotlinx.android.synthetic.main.activity_data_input.*
 
 
-
 class DataInput : AppCompatActivity() {
-//   override fun onSaveInstanceState(savedInstanceState: Bundle) {
-//        super.onSaveInstanceState(savedInstanceState)
-//        savedInstanceState.putString("androidname", edtNameAndroid.text.toString())
-//        savedInstanceState.putString("iosname", edtNameIOS.text.toString())
-//        savedInstanceState.putString("androidscore", tvScoreAndroid.text.toString())
-//        savedInstanceState.putString("iodscore", tvScoreIOS.text.toString())
-//  }
-//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-//        super.onRestoreInstanceState(savedInstanceState);
-//        edtNameAndroid.text = savedInstanceState.get("androidname") as Editable
-//        tvScoreAndroid.text = savedInstanceState.get("androidscore") as Editable
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putString("nameandroid", edtNameAndroid.text.toString())
+        savedInstanceState.putString("scoreandroi", tvScoreAndroid.text.toString())
+        savedInstanceState.putString("nameios", edtNameIOS.text.toString())
+        savedInstanceState.putString("scoreios", tvScoreIOS.text.toString())
+    }
 
- //   }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        tvScoreAndroid.text = savedInstanceState.getString("scoreandroi")
+        tvScoreIOS.text = savedInstanceState.getString("scoreios")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_input)
-        val scoreOOP = NameScoreOOP(NameInputandroid = "",ScoreInPutandroid = 0,NameInPutIOS = "",ScoreInPutIOS = 0)
+        val scoreOOP = NameScoreOOP(
+            NameInputandroid = "",
+            ScoreInPutandroid = 0,
+            NameInPutIOS = "",
+            ScoreInPutIOS = 0
+        )
         // BTN SET ONCLICK LISTENNER
         btnINCAndroid.setOnClickListener {
             scoreOOP.androiScoreINC()
@@ -44,22 +50,23 @@ class DataInput : AppCompatActivity() {
         }
         btnDECIOS.setOnClickListener {
             scoreOOP.IOSScoreDEC()
-            tvScoreIOS.text = scoreOOP.ScoreInPutIOS.toString() }
-
-            btnDone.setOnClickListener {
-                scoreOOP.NameInputandroid = edtNameAndroid.text.toString()
-                scoreOOP.NameInPutIOS = edtNameAndroid.text.toString()
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("AndroidName", edtNameAndroid.text.toString())
-                intent.putExtra("IOSName", edtNameIOS.text.toString())
-                intent.putExtra("AndroidScore", tvScoreAndroid.text.toString())
-                intent.putExtra("IOSScore", tvScoreIOS.text.toString())
-                setResult(Activity.RESULT_OK,intent)
-                finish();
-                Log.d("AAA", "BTN DONE ON")
-            }
-
+            tvScoreIOS.text = scoreOOP.ScoreInPutIOS.toString()
         }
+
+        btnDone.setOnClickListener {
+            scoreOOP.NameInputandroid = edtNameAndroid.text.toString()
+            scoreOOP.NameInPutIOS = edtNameAndroid.text.toString()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("nameandroidSend", edtNameAndroid.text.toString())
+            intent.putExtra("nameiosSend", edtNameIOS.text.toString())
+            intent.putExtra("scoreandroidSend", tvScoreAndroid.text.toString())
+            intent.putExtra("scoreiosSend", tvScoreIOS.text.toString())
+            setResult(Activity.RESULT_OK, intent)
+            finish();
+            Log.d("AAA", "BTN DONE ON")
+        }
+
     }
+}
 
 
--- app/src/main/java/com/example/learn09082021v001/MainActivity.kt
+++ app/src/main/java/com/example/learn09082021v001/MainActivity.kt
@@ -1,38 +1,66 @@
 package com.example.learn09082021v001
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.android.synthetic.main.activity_data_input.*
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putString("nameandroidView", tvViewNameAndroid.text.toString())
+        savedInstanceState.putString("scoreandroidView", tvViewScoreAndroid.text.toString())
+        savedInstanceState.putString("nameiosView", tvViewNameIOS.text.toString())
+        savedInstanceState.putString("scoreiosView", tvViewScoreIOS.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        tvViewNameAndroid.text = savedInstanceState.getString("nameandroidView")
+        tvViewScoreAndroid.text = savedInstanceState.getString("scoreandroidView")
+        tvViewNameIOS.text = savedInstanceState.getString("nameiosView")
+        tvViewScoreIOS.text = savedInstanceState.getString("scoreiosView")
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data != null && resultCode == RESULT_OK) {
+            tvViewNameAndroid.setText(data.getStringExtra("nameandroidSend"))
+            tvViewNameIOS.setText(data.getStringExtra("nameiosSend"))
+            tvViewScoreAndroid.setText(data.getStringExtra("scoreandroidSend"))
+            tvViewScoreIOS.setText(data.getStringExtra("scoreiosSend"))
+            datacheck()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // DATA GETEXTRA
-        val intent = getIntent()
-        tvViewNameAndroid.setText(intent.getStringExtra("AndroidName"))
-        tvViewNameIOS.setText(intent.getStringExtra("IOSName"))
-        tvViewScoreAndroid.setText(intent.getStringExtra("AndroidScore"))
-        tvViewScoreIOS.setText(intent.getStringExtra("IOSScore"))
-        if (tvViewNameAndroid.text.isNotEmpty() && tvViewNameIOS.text.isNotEmpty()) {
-            btnStartReset.setText("RESET")
-        } else {
-            btnStartReset.setText("START")
-        }
         btnStartReset.setOnClickListener {
-            if (tvViewNameAndroid.text.isEmpty() && tvViewNameIOS.text.isEmpty()) {
+            if (tvViewNameAndroid.text.isEmpty() && tvViewNameIOS.text.isEmpty() &&
+                    tvViewScoreAndroid.text.isEmpty()  && tvViewScoreIOS.text.isEmpty()) {
                 val intent: Intent = Intent(this, DataInput::class.java)
-                startActivity(intent)
-            } else {
-
-                tvViewNameAndroid.setText("")
-                tvViewNameIOS.setText("")
-                tvViewScoreAndroid.setText("")
-                tvViewScoreIOS.setText("")
-                btnStartReset.setText("START")
-            }
+                startActivityForResult(intent, 1)
+            } else { dataclear () }
         }
+
 
     }
+    fun datacheck () {
+        if (tvViewNameAndroid.text.isNotEmpty() || tvViewNameIOS.text.isNotEmpty()
+            || tvViewScoreAndroid.text.isNotEmpty() || tvViewScoreIOS.text.isNotEmpty()) {
+            btnStartReset.setText("RESET") } else btnStartReset.setText("START") }
+    fun dataclear (){
+        tvViewNameAndroid.setText("")
+        tvViewNameIOS.setText("")
+        tvViewScoreAndroid.setText("")
+        tvViewScoreIOS.setText("")
+        btnStartReset.setText("START") }
 }
